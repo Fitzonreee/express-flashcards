165dbfb67381682d6b5153f07dcf897e6b6d1f06
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-06-21 08:54:03 -0700

    Playing with routes, finished 'Getting Started with Express' section

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("<h1>Kevin is super cool!</h1>")
+});
+
+app.get('/hello', (req, res) => {
+  res.send("<h1>Hello, JS Devloper!</h1>")
+});
+
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000');
+});
