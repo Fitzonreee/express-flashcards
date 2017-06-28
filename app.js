4728b10e173be14cb2e429ff7298af1208986ef3
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-06-27 17:48:42 -0700

    Added a /hello post route, get route and template to post a username to the page

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
 const app = express();
+
+const cardData = {
+  prompt: "Who is Luke's father?",
+  hint: "It ryhmes with Garth Day-der"
+}
 
 app.set('view engine', 'pug');
 
@@ -8,13 +12,17 @@ app.get('/', (req, res) => {
   res.render('index')
 });
 
-const cardData = {
-  prompt: "Who is Luke's father?",
-  hint: "It ryhmes with Garth Day-der"
-}
-
 app.get('/cards', (req, res) => {
   res.render('card', cardData)
+});
+
+app.get('/hello', (req, res) => {
+  res.render('hello')
+});
+
+app.post('/hello', (reg, res) => {
+  // console.log("Submitted!");
+  res.render('hello')
 });
 
 app.listen(3000, () => {
