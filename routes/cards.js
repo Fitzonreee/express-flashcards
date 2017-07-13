7f0e42400a6298308bb745ec345e04d00053e798
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-07-13 08:21:45 -0700

    Added button on cards page to toggle between Question and Answer

--- routes/cards.js
+++ routes/cards.js
@@ -3,16 +3,27 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+// route is /cards
+router.get('/', (req, res) => {
+
+});
+
+// routes is /cards/id
 router.get('/:id', (req, res) => {
   const { side } = req.query;
   const { id } = req.params;
   const text = cards[id][side];
   const { hint } = cards[id];
 
-  const templateData = { text };
+  const templateData = { id, text };
 
   if (side === 'question') {
     templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
+  } else if (side === 'answer') {
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
   }
 
   console.log(req.query);
